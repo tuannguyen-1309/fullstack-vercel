c717f6735c9e2a277787bee01ba5ec1588129660
tuannguyen-1309 <tuandungyn2@gmail.com>
2024-12-05 21:02:36 +0700

    init

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,15 +3,18 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-    return {
-        plugins: [react()],
-        server: mode === "development" && {
-            proxy: {
-                "/api": "http://localhost:5000", // Proxy chỉ sử dụng khi chạy local
-            },
-        },
-        build: {
-            outDir: "../dist", // Đảm bảo thư mục build nằm bên ngoài thư mục frontend
-        },
-    };
+  return {
+    plugins: [react()],
+    server: mode === "development" && {
+      proxy: {
+        "/api": "http://localhost:5000", // Proxy chỉ sử dụng khi chạy local
+      },
+    },
+    build: {
+      outDir: "../dist", // Đảm bảo thư mục build nằm bên ngoài thư mục frontend
+      rollupOptions: {
+        external: ["bootstrap/dist/css/bootstrap.min.css"],
+      },
+    },
+  };
 });
